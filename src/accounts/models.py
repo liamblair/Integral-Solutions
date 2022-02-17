59ccc6dd55620c4c67fa2022f1db6c94fd9ca50c
liamblair <liam.blair@maine.edu>
2022-02-17 12:56:52 -0500

    working on user model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,6 +1,18 @@
+from winreg import REG_EXPAND_SZ
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+from .managers import UserManager
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('Email Address'), unique=True)
+    name = models.CharField(max_length=150)
     
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS =['name']
--- src/posas/settings.py
+++ src/posas/settings.py
@@ -134,3 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+AUTH_USER_MODEL = 'accounts.User'
