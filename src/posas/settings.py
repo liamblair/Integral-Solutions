846802e2507c838b296ef1d87e85b2c1c9fb1125
MackenzieCreamer <mackenzie.creamer@maine.edu>
2022-02-23 12:37:35 -0500

    Comment settings.py

--- src/posas/settings.py
+++ src/posas/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from typing import cast
 from decouple import config
+
+#For static folder stuffs
 import os
 import socket
 
@@ -126,6 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+#Code from online for handling the logic of 
+#from where static files should be getting pulled
 STATIC_URL = '/static/'
 HOSTNAME = socket.gethostname()
 
